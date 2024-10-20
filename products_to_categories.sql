5189bbcc8bc1b84849b8864d373e72638285a434
PhsPhD <51534022+phsphd@users.noreply.github.com>
2024-10-20 12:14:21 -0400

    Update products_to_categories.sql

--- products_to_categories.sql
+++ products_to_categories.sql
@@ -23,3 +23,47 @@ JOIN products_to_categories ptc ON ptc.products_id = wp.ID
 JOIN categories_description cd ON cd.categories_id = ptc.categories_id
 JOIN wp_terms wt ON wt.name = cd.categories_name
 JOIN wp_term_taxonomy wtt ON wtt.term_id = wt.term_id AND wtt.taxonomy = 'product_cat';
+
+-- Find duplicate category names and their associated term_ids
+SELECT name, MIN(term_id) as keep_term_id, GROUP_CONCAT(term_id) as duplicate_term_ids
+FROM wp_terms
+GROUP BY name
+HAVING COUNT(*) > 1;
+ UPDATE wp_term_relationships wtr
+JOIN wp_term_taxonomy wtt ON wtr.term_taxonomy_id = wtt.term_taxonomy_id
+JOIN (
+    SELECT wt.name, MIN(wt.term_id) AS keep_term_id, GROUP_CONCAT(wt.term_id) AS duplicate_term_ids
+    FROM wp_terms wt
+    GROUP BY wt.name
+    HAVING COUNT(*) > 1
+) dup ON wtt.term_id = dup.keep_term_id
+SET wtr.term_taxonomy_id = (
+    SELECT wtt2.term_taxonomy_id
+    FROM wp_term_taxonomy wtt2
+    WHERE wtt2.term_id = dup.keep_term_id AND wtt2.taxonomy = 'product_cat'
+)
+WHERE FIND_IN_SET(wtt.term_id, dup.duplicate_term_ids);
+
+
+DELETE wtt
+FROM wp_term_taxonomy wtt
+JOIN (
+    SELECT wt.name, MIN(wt.term_id) AS keep_term_id, GROUP_CONCAT(wt.term_id) AS duplicate_term_ids
+    FROM wp_terms wt
+    GROUP BY wt.name
+    HAVING COUNT(*) > 1
+) dup ON FIND_IN_SET(wtt.term_id, dup.duplicate_term_ids)
+WHERE wtt.term_id != dup.keep_term_id
+AND wtt.taxonomy = 'product_cat';
+
+-- Reassign duplicate term references in wp_term_taxonomy
+UPDATE wp_term_taxonomy wtt
+JOIN (
+    SELECT wt.name, MIN(wt.term_id) AS keep_term_id, GROUP_CONCAT(wt.term_id) AS duplicate_term_ids
+    FROM wp_terms wt
+    GROUP BY wt.name
+    HAVING COUNT(*) > 1
+) dup ON FIND_IN_SET(wtt.term_id, dup.duplicate_term_ids)
+SET wtt.term_id = dup.keep_term_id
+WHERE FIND_IN_SET(wtt.term_id, dup.duplicate_term_ids);
+
